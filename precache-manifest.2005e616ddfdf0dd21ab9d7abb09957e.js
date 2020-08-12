5bb23490ae3be3d5966de324ea2582a51de40470
Ben K <ben.knight.brazil@hotmail.com>
2020-08-11 20:32:47 -0600

    Updates

---
+++ precache-manifest.2005e616ddfdf0dd21ab9d7abb09957e.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "430e2bfaced93f657af74ae31070a9fd",
+    "url": "/reactPortfolio/index.html"
+  },
+  {
+    "revision": "0f68d1a438ffbb7bef6f",
+    "url": "/reactPortfolio/static/css/2.6bfe82aa.chunk.css"
+  },
+  {
+    "revision": "66b862bea1fc3a9a53d4",
+    "url": "/reactPortfolio/static/css/main.49beaa1b.chunk.css"
+  },
+  {
+    "revision": "0f68d1a438ffbb7bef6f",
+    "url": "/reactPortfolio/static/js/2.09b9ef5b.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/reactPortfolio/static/js/2.09b9ef5b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "66b862bea1fc3a9a53d4",
+    "url": "/reactPortfolio/static/js/main.98be9bb8.chunk.js"
+  },
+  {
+    "revision": "54f34b9d82f1dc7db405",
+    "url": "/reactPortfolio/static/js/runtime-main.1f80b427.js"
+  },
+  {
+    "revision": "6cfe42fee6a470a190ffcac89427e27e",
+    "url": "/reactPortfolio/static/media/BEn.6cfe42fe.jpg"
+  },
+  {
+    "revision": "83b53ff398acd576dd2c6b034b8e2627",
+    "url": "/reactPortfolio/static/media/BenKChop.83b53ff3.jpg"
+  }
+]);
